7f7c8a906f8005f3ceb2bec6b578e4862afe6e27
Paige Niedringhaus <paigen11@gmail.com>
2020-06-01 15:47:24 -0400

    updating types of form submissions

--- site/meadowlark.js
+++ site/meadowlark.js
@@ -2,8 +2,11 @@ const express = require('express');
 const expressHandlebars = require('express-handlebars');
 const helmet = require('helmet');
 const bodyParser = require('body-parser');
+const multiparty = require('multiparty');
+
 const handlers = require('./lib/handlers');
 const weatherMiddlware = require('./lib/middleware/weather');
+const { credentials } = require('./config');
 
 const app = express();
 
@@ -11,9 +14,6 @@ const app = express();
 app.disable('x-powered-by');
 // adds more express security features
 app.use(helmet());
-
-// makes req.body available to us
-app.use(bodyParser.urlencoded({ extended: true }));
 
 // configure Handlebars view enginer
 app.engine(
@@ -32,6 +32,10 @@ app.engine(
 );
 app.set('view engine', 'handlebars');
 
+// makes req.body available to us
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 const port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/public'));
@@ -45,17 +49,44 @@ app.get('/', handlers.home);
 // about page route
 app.get('/about', handlers.about);
 
+// newsletter sign-up page, processing function, and thank you page
+app.get('/newsletter-signup', handlers.newsletterSignup);
+app.post('/newsletter-signup/process', handlers.newsletterSignupProcess);
+app.get('/newsletter-signup/thank-you', handlers.newsletterSignupThankYou);
+
+// handlers for fetch/JSON form submission
+app.get('/newsletter', handlers.newsletter);
+app.post('/api/newsletter-signup', handlers.api.newsletterSignup);
+
+// vacation photo contest
+app.get('/contest/vacation-photo', handlers.vacationPhotoContest);
+app.get('/contest/vacation-photo-ajax', handlers.vacationPhotoContestAjax);
+app.post('/contest/vacation-photo/:year/:month', (req, res) => {
+  const form = new multiparty.Form();
+  form.parse(req, (err, fields, files) => {
+    if (err) return res.status(500).send({ error: err.message });
+    handlers.vacationPhotoContestProcess(req, res, fields, files);
+  });
+});
+app.get(
+  '/contest/vacation-photo-thank-you',
+  handlers.vacationPhotoContestProcessThankYou,
+);
+app.post('/api/vacation-photo-contest/:year/:month', (req, res) => {
+  const form = new multiparty.Form();
+  form.parse(req, (err, fields, files) => {
+    if (err)
+      return handlers.api.vacationPhotoContestError(req, res, err.message);
+    handlers.api.vacationPhotoContest(req, res, fields, files);
+  });
+});
+
 // custom 404
 // app.use is method by which Express adds middleware
 app.use(handlers.notFound);
 
 // custom 500
 app.use(handlers.serverError);
-
-// newsletter sign-up page, processing function, and thank you page
-app.get('/newsletter-signup', handlers.newsletterSignup);
-app.post('/newsletter-signup/process', handlers.newsletterSignupProcess);
-app.get('/newsletter-signup/thank-you', handlers.newsletterSignupThankYou);
 
 // if you run a JS file directly with node, require.main will equal the global module,
 // otherwise it's being imported from another module
